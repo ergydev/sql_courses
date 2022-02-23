a6813729242d19e11fb3fddd03e6a6a8667ad3f9
ergydev <ergyzenzo.dev@gmail.com>
2022-02-23 16:32:16 +0100

    added exo

--- 02-imbriquee_et_jointures/imbriquee_et_jointures.sql
+++ 02-imbriquee_et_jointures/imbriquee_et_jointures.sql
@@ -224,9 +224,147 @@ GROUP BY prenom;
 +--------+-----------+
 
 -- Exercice : qui a emprunté le plus de livre
-
-SELECT  prenom 
+SELECT COUNT(id_emprunt) AS nombre , prenom 
 FROM emprunt, abonne
 WHERE abonne.id_abonne = emprunt.id_abonne
-ORDER BY prenom
-ASC LIMIT 1;
+GROUP BY emprunt.id_abonne;
+ORDER BY COUNT(id_emprunt) DESC;
+
+
+
+
+
+-- EXERCICE : nous aimerions connaitre le nombre de livre à rendre pour chaque abonné 
+SELECT COUNT(id_emprunt) AS nombre,  prenom 
+FROM emprunt, abonne
+WHERE emprunt.id_abonne = abonne.id_abonne 
+AND date_rendu IS NULL 
+GROUP BY emprunt.id_abonne;
++--------+--------+
+| nombre | prenom |
++--------+--------+
+|      1 | Benoit |
+|      1 | Chloe  |
++--------+--------+
+
+-- EXERCICE : Qui a emprunté quoi et quand ? 
+
+SELECT prenom , titre , date_sortie
+FROM abonne, livre , emprunt
+WHERE abonne.id_abonne = emprunt.id_abonne  
+AND livre.id_livre = emprunt.id_livre;
++-----------+-------------------------+-------------+
+| prenom    | titre                   | date_sortie |
++-----------+-------------------------+-------------+
+| Guillaume | Une vie                 | 2016-12-07  |
+| Guillaume | La Reine Margot         | 2016-12-15  |
+| Benoit    | Bel-Ami                 | 2016-12-07  |
+| Benoit    | Les Trois Mousquetaires | 2017-01-02  |
+| Benoit    | Une vie                 | 2017-02-20  |
+| Chloe     | Une vie                 | 2016-12-11  |
+| Chloe     | Les Trois Mousquetaires | 2017-02-15  |
+| Laura     | Le Petit chose          | 2016-12-12  |
++-----------+-------------------------+-------------+
+
+
+---------------------------------------------------------------------
+--------------------------------------------------------------------
+-- REQUETES EN JOINTURE EXTERNE ( Sans correspondance exigée)
+---------------------------------------------------------------------
+-- Exercice : afficher la liste des abonnées et les id_livre qu'ils ont emprunté
+
+SELECT prenom , id_livre
+FROM abonne , emprunt
+WHERE abonne.id_abonne = emprunt.id_abonne;
++-----------+----------+
+| prenom    | id_livre |
++-----------+----------+
+| Guillaume |      100 |
+| Benoit    |      101 |
+| Chloe     |      100 |
+| Laura     |      103 |
+| Guillaume |      104 |
+| Benoit    |      105 |
+| Chloe     |      105 |
+| Benoit    |      100 |
++-----------+----------+
+
+-- Exercice : Rajoutez-vous dans la table abonne 
+
+INSERT INTO abonne 
+(prenom) 
+VALUES ('Ergy') ;
+
+-- Affichez tous les abonnés sans exception ainsi que les id_livre qu'ils ont emprunté si c'est le cas !
+-- si on déclenche la requête précédente (les prenom et les id_livre) nous ne somme pas présent dans le résultat car nous n'avons pas fait d'emprunt.
+-- La jointure externe nous permet de donner la priorité à une table afin de récupérer tout son contenu puis de rajouter le contenu d'autres tables sur la  base de la relation entre ces tables.
+SELECT prenom, id_livre FROM abonne LEFT JOIN emprunt ON abonne.id_abonne = emprunt.id_abonne; -- priorité sur abonné car à gauche
+
+-----------------
+
+SELECT prenom, id_livre FROM emprunt RIGHT JOIN abonne ON abonne.id_abonne = emprunt.id_abonne; -- priorité sur abonné car à droite
+
+-- On peut utiliser LEFT JOIN ou RIGHT JOIN
+-- LEFT JOIN donne la priorité à la table considérée comme étant à gauche dans le sens de lecture de la requete
+-- RIGHT JOIN donne la priorité à la table considérée comme étant à droite dans le sens de lecture de la requete
+
+-- Pour préciser la relation entre les deux tables avec un left ou right join, on peut utiliser ON colonne = colonne ou USING(colonne) si elles ont les mêmes noms sur les deux tables.
+SELECT prenom, id_livre FROM abonne LEFT JOIN emprunt USING(id_abonne);
+
++-----------+----------+
+| prenom    | id_livre |
++-----------+----------+
+| Guillaume |      100 |
+| Benoit    |      101 |
+| Chloe     |      100 |
+| Laura     |      103 |
+| Guillaume |      104 |
+| Benoit    |      105 |
+| Chloe     |      105 |
+| Benoit    |      100 |
+| Ergy      |     NULL |
++-----------+----------+
+
+-- EXERCICE : affichez tous les livres sans exception puis les id_abonne qui les ont emprunté si c'est la cas
+
+SELECT titre, id_abonne  
+FROM livre 
+LEFT JOIN emprunt 
+USING (id_livre);
++-------------------------+-----------+
+| titre                   | id_abonne |
++-------------------------+-----------+
+| Une vie                 |         1 |
+| Bel-Ami                 |         2 |
+| Une vie                 |         3 |
+| Le Petit chose          |         4 |
+| La Reine Margot         |         1 |
+| Les Trois Mousquetaires |         2 |
+| Les Trois Mousquetaires |         3 |
+| Une vie                 |         2 |
+| Le p?re Goriot          |      NULL |
++-------------------------+-----------+
+
+-- EXERCICE : Donnez le nombre de livre à rendre pour chaque abonné . Les abonnés n'ayant pas fait d'emprunt doivent apparaitre : 
+SELECT COUNT(id_emprunt) AS nombre,  prenom 
+FROM emprunt, abonne
+WHERE emprunt.id_abonne = abonne.id_abonne 
+AND date_rendu IS NULL 
+GROUP BY emprunt.id_abonne;
+
+SELECT COUNT(id_emprunt) AS nombre, prenom
+FROM abonne 
+LEFT JOIN emprunt
+ON emprunt.id_abonne = abonne.id_abonne
+AND date_rendu IS NULL
+GROUP BY prenom;
+
++--------+-----------+
+| nombre | prenom    |
++--------+-----------+
+|      1 | Benoit    |
+|      1 | Chloe     |
+|      0 | Ergy      |
+|      0 | Guillaume |
+|      0 | Laura     |
++--------+-----------+
